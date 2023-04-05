cc2440576f0905edcdd9f444e9a9a3b8c7c0fa1f
muhindokiro <blaize1143@gmail.com>
2023-04-05 16:10:16 +0300

    add method to add file

--- src/app/core/services/files.service.ts
+++ src/app/core/services/files.service.ts
@@ -18,5 +18,7 @@ export class FilesService {
     // @ts-ignore
     return this.http.post(this.baseUrl + '/files', query);
   }
-
+  createFile(payload:any):Observable<any>{
+    return this.http.post(this.baseUrl + '/new_file', payload)
+  }
 }
